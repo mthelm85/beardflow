6719017052f9a08cc6a8a4e500299b7ba6a12f19
Matt Helm <mthelm85@gmail.com>
2018-05-07 12:40:06 -0500

    added profile layout

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,19 +1,53 @@
 <template lang="html">
-  <div class="container h-100">
+  <div class="container-fluid h-100">
     <div class="row h-100">
-      <div class="col-3">
+      <div class="col-3 mt-3">
         <div class="card">
           <img class="card-img-top" src="../assets/6.jpeg">
           <div class="card-body">
             <h5 class="card-title">Matt Helm</h5>
+            <p>BeardFlow Creator</p>
           </div>
         </div>
       </div>
-      <div class="col-6 bg-secondary">
-        Fat Middle
+      <div class="col-6 mt-3">
+        <div class="list-group list-group-flush">
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">My fav new beard balm recipe</h5>
+              <small>2 days ago</small>
+            </div>
+            <p class="mb-1">I've made a lot of different beard balms and waxes but this...</p>
+            <small>By BairdBeardsley</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">How to cut cheek lines</h5>
+              <small class="text-muted">3 days ago</small>
+            </div>
+            <p class="mb-1">You don't need to go to a professional barber to get perfect cheek lin...</p>
+            <small class="text-muted">By GOtee</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">Finally, a good homemade beard wash</h5>
+              <small class="text-muted">4 days ago</small>
+            </div>
+            <p class="mb-1">Beard washes/shampoos and conditioners can get costly...</p>
+            <small class="text-muted">By FuManchu</small>
+          </a>
+        </div>
+        <div class="card-footer">
+          <a href="#">Post your own story...</a>
+        </div>
       </div>
-      <div class="col-3">
-        Right Side
+      <div class="col-3 mt-3">
+        <div class="list-group">
+          <p class="list-group-item bg-dark text-white">Cool Beard Stuff</p>
+          <a href="#" class="list-group-item list-group-item-action">Face Analysis</a>
+          <a href="#" class="list-group-item list-group-item-action">Beard Time Lapse</a>
+          <a href="#" class="list-group-item list-group-item-action">Create/Store Recipes</a>
+        </div>
       </div>
     </div>
   </div>
