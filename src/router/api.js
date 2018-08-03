f43d99e38b3cd13c6fcc3bf9b3c68b8c9ceb111d
Matt Helm <mthelm85@gmail.com>
2018-08-03 05:56:22 -0500

    changed footer e-mail, api route

--- src/router/api.js
+++ src/router/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'https://beardflow.herokuapp.com/api',
+    baseURL: 'https://www.beardflow.app/api',
     withCredentials: true
   })
 }
